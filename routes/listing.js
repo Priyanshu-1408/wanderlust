9ca5417785d83e3c6a38fdf8317baa4028d8771a
priyanshu kumar <priyanshu.official.1408@gmail.com>
2025-06-28 15:05:35 +0530

    add a image directly

--- routes/listing.js
+++ routes/listing.js
@@ -1,57 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-
 const Listing = require("../models/listing.js");
 const {isLoggedIn , isOwner , validateListing} = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer( {storage });
 
+router.route("/")
+.get(wrapAsync(listingController.index))
+// .post(
+//        isLoggedIn, validateListing, 
+//       wrapAsync(listingController.createListing)
+      
+// );
+.post( upload.single('listing[image]'),(req , res)=>{
+  res.send(req.file);
+});
 
-
-
-
-// index route
-router.get("/" ,wrapAsync(listingController.index));
-
-
-  // new route
+// new route
   router.get("/new" , isLoggedIn, listingController.renderNewForm);
 
+router.route("/:id")
+.get( wrapAsync(listingController.showListing))
+.put(isLoggedIn ,isOwner,
+      validateListing,
+      wrapAsync(listingController.updateListing))
+      .delete(isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+
+
+
+
+
+  
+
   // show route
-  router.get("/:id" , wrapAsync(listingController.showListing));
+  
 
-  //create route
-//   router.post(
-//   "/login",
-//   passport.authenticate("local", {
-//     failureRedirect: "/login",
-//     failureFlash: true,
-//   }),
-//   (req, res) => {
-//     // Use saved redirectUrl after login
-//     const redirectUrl = req.session.redirectUrl || "/listings";
-//     delete req.session.redirectUrl; // Clean up session
-//     res.redirect(redirectUrl);
-//   }
-// );
+  
 
-   //create route
-    router.post(
-      "/" , isLoggedIn, validateListing, 
-      wrapAsync(listingController.createListing)
-      
-    ); 
+   
   
     //edit route
     router.get("/:id/edit" , isLoggedIn ,isOwner, wrapAsync(listingController.renderEditForm));
   
-    // update route
-    router.put("/:id" ,isLoggedIn ,isOwner,
-      validateListing,
-      wrapAsync(listingController.updateListing));
   
-    // delete route
-    router.delete("/:id" ,isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+  
+
 
   module.exports = router;
