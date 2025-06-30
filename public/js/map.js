7a53f8bc2e64c0df65edb9a95ee3e9d09b8c5fe8
priyanshu kumar <priyanshu.official.1408@gmail.com>
2025-06-30 10:15:50 +0530

    map location feature is fully functional

--- public/js/map.js
+++ public/js/map.js
@@ -3,7 +3,17 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [77.2090, 28.6139], // starting position [lng, lat]
+style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
+center: listing.geometry.coordinates, // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
+
+
+const marker = new mapboxgl.Marker({color : "blue"})
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be Provided After Booking</p>`)
+    )
+        .addTo(map);
+
